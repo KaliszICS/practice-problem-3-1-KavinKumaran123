1b4dd8392c4615aa8032fe93ad3d250c345937ff
KavinKumaran123 <166553090+KavinKumaran123@users.noreply.github.com>
2026-04-06 21:38:15 +0000

    3.1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,22 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static int sumOfDigits(int x) {
+        if (x < 0)
+            x = -x;
 
-	}
+        if (x <= 9)
+            return x;
 
-	
+        return (x % 10) + sumOfDigits(x / 10);
+    }
 
+    public static int factorial(int n) {
+        if (n < 0)
+            return 0;
+
+        if (n == 0)
+            return 1;
+
+        return n * factorial(n - 1);
+    }
 }
